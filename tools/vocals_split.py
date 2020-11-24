00322cd2e8138f18cb690fbcd53cdc7f4a7a5029
Carlos <carloslariossolis@gmail.com>
2020-11-23 21:33:59 -0800

    feat: Split vocals using Spleeter

---
+++ tools/vocals_split.py
@@ -0,0 +1,18 @@
+from spleeter.separator import Separator
+import os
+from pathlib import Path
+import glob
+import sys
+
+basePath = Path(__file__).parent
+songsPath = "../songs/*.mp3"
+outputPath = "../output"
+songsPath = (basePath / songsPath).resolve()
+outputPath = (basePath / outputPath).resolve()
+
+songs = glob.glob(str(songsPath))
+
+outputPath = str(outputPath)
+separator = Separator('spleeter:2stems')
+for song in songs:
+    separator.separate_to_file(song, outputPath)
