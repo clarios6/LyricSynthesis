e81108b95abdd0183435d4813ae20bafde7f8c1c
Carlos <carloslariossolis@gmail.com>
2020-11-23 18:07:12 -0800

    bugfix: Make masterfiles on Linux

--- tools/generate_master_lyrics.py
+++ tools/generate_master_lyrics.py
@@ -7,11 +7,13 @@ print(sys.getdefaultencoding())
 basePath = Path(__file__).parent
 lyricsPath = "../lyrics"
 lyricType = "*.txt"
-fullPath = (basePath / lyricsPath).resolve()
+masterFile = "masterLyricFile.txt"
+songsPath = (basePath / lyricsPath / lyricType).resolve()
+masterPath = (basePath / lyricsPath / masterFile).resolve()
 
-songs = glob.glob("../lyrics/*.txt")
+songs = glob.glob(str(songsPath))
 
-with open("../lyrics/masterLyricFile.txt", encoding='utf-8', mode="w") as master:
+with open(masterPath, encoding='utf-8', mode="w") as master:
     for song in songs:
         with open(song, encoding='utf-8', mode="r") as fp:
             master.write(fp.read())
